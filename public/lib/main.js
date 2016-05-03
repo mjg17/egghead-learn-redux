cd6e2aa498ebd31e1d8de7f0efe9f9d5beaebb0c
Michael Gray <mg13@sanger.ac.uk>
2016-05-03 12:59:40 +0100

    main.js: get counter() from lib/reducers/

--- public/lib/main.js
+++ public/lib/main.js
@@ -1,14 +1,4 @@
-      // counter is the reducer (function) that manages state updates:
-      const counter = (state = 0, action) => {
-        switch (action.type) {
-          case 'INCREMENT':
-            return state + 1;
-          case 'DECREMENT':
-            return state - 1;
-          default:
-            return state;
-        }
-      }
+import counter from 'lib/reducers/counter';
 
       import { createStore } from 'redux';
       // create the store for our mini-app using the counter reducer
