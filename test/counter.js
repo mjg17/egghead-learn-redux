1b50b78bbec8a47721d0a90f41db76eae76c4eb7
Michael Gray <mg13@sanger.ac.uk>
2016-05-03 11:18:17 +0100

    Initial state: fix.

--- test/counter.js
+++ test/counter.js
@@ -1,6 +1,9 @@
 import expect from 'expect';
 
 function counter(state, action) {
+    if (typeof(state) === 'undefined') {
+        return 0;
+    }
     if (action.type === 'INCREMENT') {
         return state + 1;
     } else if (action.type === 'DECREMENT') {
