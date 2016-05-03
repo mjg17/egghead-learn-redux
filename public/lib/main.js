f933b54ba7433d3fc662e5909448b60deddc6f00
Michael Gray <mg13@sanger.ac.uk>
2016-05-03 13:01:18 +0100

    main.js: indentation.

--- public/lib/main.js
+++ public/lib/main.js
@@ -1,16 +1,16 @@
 import counter from 'lib/reducers/counter';
 
-      import { createStore } from 'redux';
-      // create the store for our mini-app using the counter reducer
-      const store = createStore(counter);
+import { createStore } from 'redux';
+// create the store for our mini-app using the counter reducer
+const store = createStore(counter);
 
-      const render = () => { // render function updates DOM with counter value
-        document.body.innerText = store.getState();
-      }
-      store.subscribe(render); // all actions re-render the DOM
-      render(); // render the initial state of the page/app
+const render = () => { // render function updates DOM with counter value
+    document.body.innerText = store.getState();
+}
+store.subscribe(render); // all actions re-render the DOM
+render(); // render the initial state of the page/app
 
-      // listen for click event on the whole document (click anywhere on the page)
-      document.addEventListener('click', () => {
-        store.dispatch({type:'INCREMENT'});
-      });
+// listen for click event on the whole document (click anywhere on the page)
+document.addEventListener('click', () => {
+    store.dispatch({type:'INCREMENT'});
+});
