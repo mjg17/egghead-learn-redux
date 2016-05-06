c5349669e790c3cfe0855eec2693fcb24441ca71
Michael Gray <mg13@sanger.ac.uk>
2016-05-06 17:09:39 +0100

    React counter stage 1.

--- public/lib/main.js
+++ public/lib/main.js
@@ -1,12 +1,23 @@
 import counter from 'lib/reducers/counter';
 
 import { createStore } from 'redux';
+import React           from 'react';
+import ReactDOM        from 'react-dom';
+
 // create the store for our mini-app using the counter reducer
 const store = createStore(counter);
 
-const render = () => { // render function updates DOM with counter value
-    document.body.innerText = store.getState();
-}
+const Counter = ({ value }) => (
+  <h1>{value}</h1>
+);
+
+const render = () => {
+  ReactDOM.render(
+    <Counter value={store.getState()}/>,
+    document.getElementById('root')
+  );
+};
+
 store.subscribe(render); // all actions re-render the DOM
 render(); // render the initial state of the page/app
 
