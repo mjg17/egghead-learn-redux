859ebb090f70a260beea932e64a2a2e2ed23b6a9
Michael Gray <mg13@sanger.ac.uk>
2016-05-06 17:17:53 +0100

    Buttons

--- public/lib/main.js
+++ public/lib/main.js
@@ -7,21 +7,28 @@ import ReactDOM        from 'react-dom';
 // create the store for our mini-app using the counter reducer
 const store = createStore(counter);
 
-const Counter = ({ value }) => (
-  <h1>{value}</h1>
+const Counter = ({
+    value,
+    onIncrement,
+    onDecrement
+}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
 );
 
 const render = () => {
   ReactDOM.render(
-    <Counter value={store.getState()}/>,
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    />,
     document.getElementById('root')
   );
 };
 
 store.subscribe(render); // all actions re-render the DOM
 render(); // render the initial state of the page/app
-
-// listen for click event on the whole document (click anywhere on the page)
-document.addEventListener('click', () => {
-    store.dispatch({type:'INCREMENT'});
-});
