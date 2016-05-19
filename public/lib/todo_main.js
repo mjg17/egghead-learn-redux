15b171352de39fd153c143b5eab1d7e93b7a9bb8
Michael Gray <mg13@sanger.ac.uk>
2016-05-19 17:16:38 +0100

    Factor out presentational components (AddTodo) [Lesson 21]

--- public/lib/todo_main.js
+++ public/lib/todo_main.js
@@ -61,6 +61,24 @@ const TodoList = ({
     </ul>
 )
 
+const AddTodo = ({
+    onAddClick
+}) => {
+    let input;
+    return (
+        <div>
+        <input ref={node => {
+          input = node;
+        }} />
+        <button onClick={() => {
+          onAddClick(input.value);
+          input.value = '';
+        }}>
+          Add Todo
+        </button>
+        </div>
+    );
+}
 const getVisibleTodos = (
   todos,
   filter
@@ -93,19 +111,15 @@ class TodoApp extends Component {
     );
     return (
       <div>
-        <input ref={node => {
-          this.input = node;
-        }} />
-        <button onClick={() => {
-          store.dispatch({
-            type: 'ADD_TODO',
-            text: this.input.value,
-            id: nextTodoId++
-          });
-          this.input.value = '';
-        }}>
-          Add Todo
-        </button>
+        <AddTodo
+            onAddClick={
+                value => store.dispatch({
+                    type: 'ADD_TODO',
+                    text: value,
+                    id: nextTodoId++
+                })
+            }
+        />
         <TodoList
             todos={visibleTodos}
             onTodoClick={
